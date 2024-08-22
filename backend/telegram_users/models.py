b3064aa5b152c1667af36ca82374f655cc78c4ed
Iulia Zagornova <zagornowwa.julia@gmail.com>
2024-08-22 22:03:10 +0300

    create new app telegram_users

---
+++ backend/telegram_users/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class TelegramUser(models.Model):
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    username = models.CharField(max_length=100, blank=True, null=True)
